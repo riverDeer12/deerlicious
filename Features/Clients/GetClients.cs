6dffdd72ebe58221def7e037cf012e20035a0f88
Milan Trbojevic <mtrbojevic12@gmail.com>
2025-04-16 15:42:01 +0200

    finished with client endpoints, need to test them

---
+++ Features/Clients/GetClients.cs
@@ -0,0 +1,68 @@
+﻿using Deerlicious.API.Constants;
+using Deerlicious.API.Database;
+using Deerlicious.API.Features.Administrators;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Deerlicious.API.Features.Clients;
+
+public record GetClientResponse(
+    Guid Id,
+    string FirstName,
+    string LastName,
+    DateTimeOffset CreatedAt,
+    DateTimeOffset UpdatedAt,
+    bool IsDeleted,
+    ClientUserDto User);
+
+public sealed record ClientUserDto(Guid Id, string Username, string Email);
+
+public class GetClientsEndpoint : EndpointWithoutRequest<List<GetClientResponse>>
+{
+    private readonly DeerliciousContext _context;
+
+    public GetClientsEndpoint(DeerliciousContext context)
+    {
+        _context = context;
+    }
+
+    public override void Configure()
+    {
+        Get("api/clients");
+        Permissions(nameof(UserPermissions.CanGetClients));
+        Options(x => x.WithTags("Clients"));
+    }
+
+    public override async Task HandleAsync(CancellationToken cancellationToken)
+    {
+        var clients = await _context.Clients.Include(userType => userType.User)
+            .ToListAsync(cancellationToken: cancellationToken);
+
+        if (clients.Count is 0)
+        {
+            await SendAsync([], cancellation: cancellationToken);
+            return;
+        }
+
+        var response = new List<GetClientResponse>();
+
+        foreach (var client in clients)
+        {
+            var clientUser = client.User;
+
+            if (clientUser == null) continue;
+
+            var clientUserResponse = new ClientUserDto(clientUser.Id, clientUser.UserName, clientUser.Email);
+
+            var roleResponse = new GetClientResponse(client.Id,
+                client.FirstName, client.LastName,
+                client.CreatedAt,
+                client.UpdatedAt,
+                client.IsDeleted, clientUserResponse);
+
+            response.Add(roleResponse);
+        }
+
+        await SendAsync(response, cancellation: cancellationToken);
+    }
+}
